8f8bdf029309c625ef387b4222689f546417e7ec
JevonFunay <jevon.christian@ti.ukdw.ac.id>
2024-05-21 23:26:36 +0700

    1

--- Mandiri1.py
+++ Mandiri1.py
@@ -20,21 +20,21 @@ daftar_aplikasi_list = [set(aplikasi) for aplikasi in data_aplikasi.values()]
 
 hasil_intersection = daftar_aplikasi_list[0]
 for i in range(1, len(daftar_aplikasi_list)):
-    hasil_intersection = hasil_intersection&(daftar_aplikasi_list[i])
+    hasil_intersection = hasil_intersection.intersection(daftar_aplikasi_list[i])
 
 print("Aplikasi yang ada di semua kategori:", hasil_intersection)
 
 satukategori = daftar_aplikasi_list[0]
 for i in range(1, len(daftar_aplikasi_list)):
-    satukategori = satukategori^(daftar_aplikasi_list[i])
+    satukategori = satukategori.symmetric_difference(daftar_aplikasi_list[i])
 print("Aplikasi yang hanya ada di satu kategori:", satukategori-hasil_intersection)
 
 
 duakategori = set()
 
 for i in range(len(daftar_aplikasi_list) - 1):
-     hasil = (daftar_aplikasi_list[i] & daftar_aplikasi_list[i+1]) - satukategori
-     duakategori | hasil
-hasil2 = (daftar_aplikasi_list[0] & daftar_aplikasi_list[-1]) - satukategori
-duakategori | hasil2
+     hasil = (daftar_aplikasi_list[i].intersection(daftar_aplikasi_list[i+1])).difference(satukategori)
+     duakategori.union(hasil)
+hasil2 = (daftar_aplikasi_list[0].intersection(daftar_aplikasi_list[-1])).difference(satukategori)
+duakategori.union(hasil2)
 print("Aplikasi yang muncul tepat di 2 kategori: ", duakategori)
